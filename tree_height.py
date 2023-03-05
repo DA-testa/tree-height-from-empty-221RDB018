84513b3d9cd9386d0ed0287397dc0c6686fe5fe8
Ņina Aļjanaki <125008313+221RDB018@users.noreply.github.com>
2023-03-06 00:20:27 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,31 +1,30 @@
 # Nina Aljanaki 221RDB018
-def compute_height(n, lst):    
-    max_height = 0
-    # Your code here
-    
+def compute_height(n, lst):
     visited = [0] * n
     level = [0] * n
+    max_height = 0
 
-    for i in range(0,n):
+    for i in range(n):
         if visited[i] == 0:
             visited[i] = 1
             level[i] = 1
             stack = [i]
-            while lst[i] != -1:
-                if visited[lst[i]] == 0:
-                    visited[lst[i]] = 1
-                    stack.append(lst[i])
+            while lst[stack[-1]] != -1:
+                curr = lst[stack[-1]]
+                if visited[curr] == 0:
+                    visited[curr] = 1
+                    stack.append(curr)
                     for j in stack:
                         level[j] += 1
-                    i = lst[i]
                 else:
                     for j in stack:
-                        level[j] += level[lst[i]]
-                    lst[i] = -1
-            
+                        level[j] += level[curr]
+                    lst[stack[-1]] = -1
+                    stack.pop()
 
     max_height = max(level)
     return max_height
+
 
 def main():
     input_t = ""
@@ -55,40 +54,3 @@ def main():
     print(compute_height(n, lst)) # call the function and output it's result
 
 main()
-
-
-
-
-# # python3
-
-# import sys
-# import threading
-# import numpy
-
-
-# def compute_height(n, parents):
-#     # Write this function
-#     max_height = 0
-#     # Your code here
-#     return max_height
-
-
-# def main():
-#     # implement input form keyboard and from files
-    
-#     # let user input file name to use, don't allow file names with letter a
-#     # account for github input inprecision
-    
-#     # input number of elements
-#     # input values in one variable, separate with space, split these values in an array
-#     # call the function and output it's result
-#     pass
-
-# # In Python, the default limit on recursion depth is rather low,
-# # so raise it here for this problem. Note that to take advantage
-# # of bigger stack, we have to launch the computation in a new thread.
-# sys.setrecursionlimit(10**7)  # max depth of recursion
-# threading.stack_size(2**27)   # new thread will get stack of such size
-# threading.Thread(target=main).start()
-# main()
-# # print(numpy.array([1,2,3]))
